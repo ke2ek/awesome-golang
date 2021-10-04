5cc633caa3b30dfaed00e3f0a7911b8807ddae9b
ke2ek <ke2ek@Kwons-MacBook-Pro.local>
2021-10-05 07:58:05 +0900

    Add skiplist

---
+++ skiplist/skiplist.go
@@ -0,0 +1,19 @@
+package skiplist
+
+type Skiplist struct {
+}
+
+func New() *Skiplist {
+	return &Skiplist{}
+}
+
+func (this *Skiplist) Search(target int) bool {
+	return false
+}
+
+func (this *Skiplist) Add(num int) {
+}
+
+func (this *Skiplist) Erase(num int) bool {
+	return false
+}

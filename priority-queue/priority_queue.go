d2573f502ab593bf64ea5bfe59830e0d8107e2ad
EUNJIN KWON <gej48443@gmail.com>
2021-10-05 08:31:45 +0900

    Update priority queue

--- priority-queue/priority_queue.go
+++ priority-queue/priority_queue.go
@@ -1,8 +1,13 @@
 package priorityqueue
 
+type T interface{}
+
 type PriorityQueue struct {
 }
 
 func New() *PriorityQueue {
 	return &PriorityQueue{}
+}
+
+func (this *PriorityQueue) Push(target T) {
 }

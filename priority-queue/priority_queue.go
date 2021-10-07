b0d67ee5cda861a22ad6378d25b15cde804de541
EUNJIN KWON <gej48443@gmail.com>
2021-10-07 14:35:03 +0900

    Add priority queue

--- priority-queue/priority_queue.go
+++ priority-queue/priority_queue.go
@@ -1,13 +1,63 @@
 package priorityqueue
 
+import "awesome-golang/heap"
+
 type T interface{}
 
+type Node struct {
+	Key   int
+	Value interface{}
+}
+
+type Heapq []*Node
+
+func (this *Heapq) Len() int {
+	return len(*this)
+}
+
+func (this *Heapq) Less(i, j int) bool {
+	return (*this)[i].Key < (*this)[j].Key
+}
+
+func (this *Heapq) Swap(i, j int) {
+	(*this)[i], (*this)[j] = (*this)[j], (*this)[i]
+}
+
+func (this *Heapq) Push(x interface{}) {
+	node, ok := x.(*Node)
+	if !ok {
+		panic("Push(): the type of a given parameter is not the pointer type of Node.")
+	}
+	*this = append(*this, node)
+}
+
+func (this *Heapq) Pop() interface{} {
+	last := this.Len() - 1
+	x := (*this)[last]
+	*this = (*this)[:last]
+	return x
+}
+
 type PriorityQueue struct {
+	q *Heapq
 }
 
 func New() *PriorityQueue {
-	return &PriorityQueue{}
+	return &PriorityQueue{q: &Heapq{}}
 }
 
-func (this *PriorityQueue) Push(target T) {
+func (this *PriorityQueue) Push(key int, value interface{}) {
+	heap.Push(this.q, &Node{key, value})
+}
+
+func (this *PriorityQueue) Pop() *Node {
+	return heap.Pop(this.q).(*Node)
+}
+
+func (this *PriorityQueue) Size() int {
+	return this.q.Len()
+}
+
+func (this *PriorityQueue) Empty() bool {
+	return this.q.Len() == 0
 }
---
+++ test/priority_queue_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"awesome-golang/common"
+	priorityqueue "awesome-golang/priority-queue"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Student struct {
+	sno   int
+	name  string
+	age   int
+	major string
+}
+
+func TestPriorityQueue(t *testing.T) {
+	pq := priorityqueue.New()
+	data := map[string]Student{
+		"Park": Student{sno: 1234, name: "Park", age: 21, major: "CS"},
+		"Kim":  Student{sno: 456, name: "Kim", age: 22, major: "Economics"},
+		"Kwon": Student{sno: 42, name: "Kwon", age: 24, major: "CS"},
+		"Lee":  Student{sno: 899, name: "Lee", age: 25, major: "Physics"},
+		"Kim2": Student{sno: 3, name: "Kim", age: 22, major: "Mathmatics"},
+	}
+	for _, value := range data {
+		pq.Push(value.sno, value)
+	}
+
+	ans := []int{3, 42, 456, 899, 1234}
+	for i, num := range ans {
+		x := pq.Pop()
+		fmt.Println(i, num, x.Key)
+		assert.Equal(t, x.Key, num, common.ERROR_MSG)
+		assert.Equal(t, pq.Size(), len(ans)-1-i, common.ERROR_MSG)
+	}
+	assert.Equal(t, pq.Empty(), true, common.ERROR_MSG)
+}

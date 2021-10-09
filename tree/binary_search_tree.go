1348fbd8539de87353aa5a3a5afd6e75c9970e44
EUNJIN KWON <gej48443@gmail.com>
2021-10-09 10:46:30 +0900

    Update binsary search tree

--- tree/binary_search_tree.go
+++ tree/binary_search_tree.go
@@ -1,56 +1,69 @@
 package tree
 
 type BSTNode struct {
-	Key   int
-	Value interface{}
-	Left  *BSTNode
-	Right *BSTNode
+	Key    int
+	Value  interface{}
+	left   *BSTNode
+	right  *BSTNode
+	parent *BSTNode
 }
 
 func (this *BSTNode) getSuccessor() *BSTNode {
 	node := this
-	for node.Left != nil {
-		node = node.Left
+	for node.left != nil {
+		node = node.left
 	}
 	return node
 }
 
-func (this *BSTNode) Remove(key int) *BSTNode {
+func (this *BSTNode) replaceInParent(node *BSTNode) {
+	if this.parent != nil {
+		if this == this.parent.left {
+			this.parent.left = node
+		} else {
+			this.parent.right = node
+		}
+	}
+	if node != nil {
+		node.parent = this.parent
+	}
+}
+
+func (this *BSTNode) Remove(key int) {
 	if key < this.Key {
-		this.Left = this.Left.Remove(key)
+		this.left.Remove(key)
 	} else if key > this.Key {
-		this.Right = this.Right.Remove(key)
+		this.right.Remove(key)
 	} else {
 		// Remove the key here.
-		if this.Left != nil && this.Right != nil {
+		if this.left != nil && this.right != nil {
 			successor := this.getSuccessor()
 			this.Key, this.Value = successor.Key, successor.Value
 			successor.Remove(successor.Key)
-		} else if this.Left != nil {
-			return this.Left
-		} else if this.Right != nil {
-			return this.Right
+		} else if this.left != nil {
+			this.replaceInParent(this.left)
+		} else if this.right != nil {
+			this.replaceInParent(this.right)
 		} else { // If there is no child
-			return nil
+			this.replaceInParent(nil)
 		}
 	}
-	return this
 }
 
 func (this *BSTNode) Add(key int, value interface{}) {
 	if key == this.Key {
 		this.Value = value
 	} else if key < this.Key {
-		if this.Left != nil {
-			this.Left.Add(key, value)
+		if this.left != nil {
+			this.left.Add(key, value)
 		} else {
-			this.Left = &BSTNode{Key: key, Value: value}
+			this.left = &BSTNode{Key: key, Value: value}
 		}
 	} else if key > this.Key {
-		if this.Right != nil {
-			this.Right.Add(key, value)
+		if this.right != nil {
+			this.right.Add(key, value)
 		} else {
-			this.Right = &BSTNode{Key: key, Value: value}
+			this.right = &BSTNode{Key: key, Value: value}
 		}
 	}
 }
@@ -58,10 +71,10 @@ func (this *BSTNode) Add(key int, value interface{}) {
 func (this *BSTNode) Search(key int) interface{} {
 	if key == this.Key {
 		return this.Value
-	} else if key < this.Key && this.Left != nil {
-		return this.Left.Search(key)
-	} else if key > this.Key && this.Right != nil {
-		return this.Right.Search(key)
+	} else if key < this.Key && this.left != nil {
+		return this.left.Search(key)
+	} else if key > this.Key && this.right != nil {
+		return this.right.Search(key)
 	}
 	return nil
 }
@@ -71,17 +84,17 @@ func TraverseInOrder(node *BSTNode, nodes *([]*BSTNode)) {
 	if node == nil {
 		return
 	}
-	TraverseInOrder(node.Left, nodes)
+	TraverseInOrder(node.left, nodes)
 	*nodes = append(*nodes, node)
-	TraverseInOrder(node.Right, nodes)
+	TraverseInOrder(node.right, nodes)
 }
 
 func SearchIteratively(node *BSTNode, key int) interface{} {
 	for node != nil {
 		if key < node.Key {
-			node = node.Left
+			node = node.left
 		} else if key > node.Key {
-			node = node.Right
+			node = node.right
 		} else {
 			return node.Value
 		}
@@ -96,7 +109,7 @@ func IsBST(node *BSTNode, minKey, maxKey int) bool {
 	if node.Key < minKey || node.Key > maxKey {
 		return false
 	}
-	return IsBST(node.Left, minKey, node.Key-1) && IsBST(node.Right, node.Key+1, maxKey)
+	return IsBST(node.left, minKey, node.Key-1) && IsBST(node.right, node.Key+1, maxKey)
 }
 
 // Return the root of the trimmed tree in which all the keys are in [low, high].
@@ -107,15 +120,15 @@ func Trim(node *BSTNode, low, high int) *BSTNode {
 	if node.Key < low {
 		// Don't have to consider the current node and the left subtree because all the keys in them are less than low.
 		// But, some keys in the right subtree can be between low and high because of the BST property.
-		return Trim(node.Right, low, high)
+		return Trim(node.right, low, high)
 	} else if node.Key > high {
 		// Likewise, the current node and the right subtree have keys greater than high.
 		// But, some keys in the left subtree can be between low and high because of the BST property.
-		return Trim(node.Left, low, high)
+		return Trim(node.left, low, high)
 	} else { // if the current key is between low and high,
 		// Alter the pointers of both subtree into the root of each trimmed tree.
-		node.Left = Trim(node.Left, low, high)
-		node.Right = Trim(node.Right, low, high)
+		node.left = Trim(node.left, low, high)
+		node.right = Trim(node.right, low, high)
 	}
 	return node
 }

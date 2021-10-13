f03905aafad620cb1d8414ec198c0b755dd35247
EUNJIN KWON <gej48443@gmail.com>
2021-10-14 08:01:39 +0900

    Add Bubble Sort

---
+++ sort/bubblesort.go
@@ -0,0 +1,12 @@
+package sort
+
+func BubbleSort(arr *[]int) {
+	N := len(*arr)
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			if (*arr)[i] < (*arr)[j] {
+				(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
+			}
+		}
+	}
+}
--- test/sort_test.go
+++ test/sort_test.go
@@ -1,16 +1,41 @@
 package test
 
 import (
-	"awesome-golang/sort"
+	mySort "awesome-golang/sort"
+	"math/rand"
+	"sort"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestQuicksort(t *testing.T) {
-	arr := []int{4, 5, 7, 8, 9, 2, 1, 6, 3}
-	sort.Quicksort(&arr, 0, len(arr)-1)
-	for i := 1; i < 10; i++ {
-		assert.Equal(t, i, arr[i-1])
+func TestBubbleSort(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	N := int(1e5)
+	arr, ans := make([]int, N), make([]int, N)
+	for i := 0; i < N; i++ {
+		arr[i] = rand.Intn(N)
+		ans[i] = arr[i]
+	}
+	sort.Ints(ans)
+	mySort.BubbleSort(&arr)
+	for i := 0; i < N; i++ {
+		assert.Equal(t, ans[i], arr[i])
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	N := int(1e5)
+	arr, ans := make([]int, N), make([]int, N)
+	for i := 0; i < N; i++ {
+		arr[i] = rand.Intn(N)
+		ans[i] = arr[i]
+	}
+	sort.Ints(ans)
+	mySort.QuickSort(&arr, 0, N-1)
+	for i := 0; i < N; i++ {
+		assert.Equal(t, ans[i], arr[i])
 	}
 }

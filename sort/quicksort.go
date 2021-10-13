4b11516965c0fbb6ed86c52d122af9613842155b
EUNJIN KWON <gej48443@gmail.com>
2021-10-14 08:01:33 +0900

    Update Quick Sort

--- sort/quicksort.go
+++ sort/quicksort.go
@@ -23,24 +23,24 @@ Time Complexity
 Worst Case: [1,2,3,4,5,6,7,8,9] -> it has been already sorted.
 			[2,2,2,2,2,2,2,2,2] -> it has had the same number.
 
-Quicksort(arr, 0, 10) -> Quicksort(arr, 0, 9) -> ... -> Quicksort(arr, 0, 1)
+QuickSort(arr, 0, 10) -> QuickSort(arr, 0, 9) -> ... -> QuickSort(arr, 0, 1)
 Iteration in partition: N -> N-1 -> ... -> 1
 Thus, it's gonna spend O(N^2) = N*(N+1)/2 = N + (N-1) + ... + 1
 
 Best Case: Every time it choose the proper index of the pivot.
 It will deal with the half of a given array every recursion to partition.
 
-Quicksort(arr, 0, 10) -> Quicksort(arr, 0, 5) -> ... -> Quicksort(arr, 0, 1)
+QuickSort(arr, 0, 10) -> QuickSort(arr, 0, 5) -> ... -> QuickSort(arr, 0, 1)
 Iteration in partition: N -> N/2 * 2 = N -> N/4 * 4 = N -> ... -> 1 * N = N
 Thus, it's gonna spend O(NlogN) because the number of recursive calls is gonna be logN.
 */
-func Quicksort(arr *[]int, l, r int) {
+func QuickSort(arr *[]int, l, r int) {
 	// base case
 	if l >= r {
 		return
 	}
 	// recursive case
 	p := partition(arr, l, r)
-	Quicksort(arr, l, p-1)
-	Quicksort(arr, p+1, r)
+	QuickSort(arr, l, p-1)
+	QuickSort(arr, p+1, r)
 }

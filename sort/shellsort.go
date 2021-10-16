edfab0b76337ce2c21d4988fa2b98867d93ced60
EUNJIN KWON <gej48443@gmail.com>
2021-10-16 21:42:50 +0900

    Add Shell Sort

---
+++ sort/shellsort.go
@@ -0,0 +1,25 @@
+package sort
+
+/*
+Shellsort is an optimization of insertion sort that allows the exchange of items that are far apart.
+The method starts by sorting pairs of elements far apart from each other,
+then progressively reducing the gap between elements to be compared.
+
+By starting with far apart elements, it can move some out-of-place elements into position faster than a simple nearest neighbor exchange.
+
+Time Complexity: O(N*log(N)^2)
+*/
+
+func ShellSort(arr *[]int) {
+	for L := len(*arr) / 2; L > 0; L /= 2 {
+		for start := L; start < len(*arr); start++ {
+			hold := (*arr)[start]
+			current := start - L
+			for current >= 0 && (*arr)[current] > hold {
+				(*arr)[current+L] = (*arr)[current]
+				current -= L
+			}
+			(*arr)[current+L] = hold
+		}
+	}
+}
--- test/sort_test.go
+++ test/sort_test.go
@@ -16,7 +16,7 @@ func TestBubbleSort(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	arr, ans := make([]int, N), make([]int, N)
 	for i := 0; i < N; i++ {
-		arr[i] = rand.Intn(N)
+		arr[i] = rand.Intn(N * 100)
 		ans[i] = arr[i]
 	}
 	sort.Ints(ans)
@@ -30,7 +30,7 @@ func TestInsertionSort(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	arr, ans := make([]int, N), make([]int, N)
 	for i := 0; i < N; i++ {
-		arr[i] = rand.Intn(N)
+		arr[i] = rand.Intn(N * 100)
 		ans[i] = arr[i]
 	}
 	sort.Ints(ans)
@@ -44,7 +44,7 @@ func TestQuickSort(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	arr, ans := make([]int, N), make([]int, N)
 	for i := 0; i < N; i++ {
-		arr[i] = rand.Intn(N)
+		arr[i] = rand.Intn(N * 100)
 		ans[i] = arr[i]
 	}
 	sort.Ints(ans)
@@ -58,11 +58,25 @@ func TestMergeSort(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	arr, ans := make([]int, N), make([]int, N)
 	for i := 0; i < N; i++ {
-		arr[i] = rand.Intn(N)
+		arr[i] = rand.Intn(N * 100)
 		ans[i] = arr[i]
 	}
 	sort.Ints(ans)
 	arr = mySort.MergeSort(&arr, 0, N-1)
+	for i := 0; i < N; i++ {
+		assert.Equal(t, ans[i], arr[i])
+	}
+}
+
+func TestShellSort(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	arr, ans := make([]int, N), make([]int, N)
+	for i := 0; i < N; i++ {
+		arr[i] = rand.Intn(N * 100)
+		ans[i] = arr[i]
+	}
+	sort.Ints(ans)
+	mySort.ShellSort(&arr)
 	for i := 0; i < N; i++ {
 		assert.Equal(t, ans[i], arr[i])
 	}
